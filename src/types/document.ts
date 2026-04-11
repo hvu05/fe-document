6fbc9035ce56b125171f31d15335d57632ca343c
NguyenThanhTrinh275 <trinh020705@gmail.com>
2026-04-11 11:09:38 +0700

    First upload file's change

---
+++ src/types/document.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+export interface UploadedDocument {
+	id: string;
+	title: string;
+	type: string;
+	fileName: string;
+	fileSize: number; // bytes
+	createdAt: Date;
+	fileUrl: string;
+}
